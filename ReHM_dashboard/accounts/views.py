a8deb8a54e34444e280ffb4bb5a34345d792f5ca
snphan <snphan@ualberta.ca>
2022-06-05 20:37:15 -0600

    use the built in redirect user

--- ReHM_dashboard/accounts/views.py
+++ ReHM_dashboard/accounts/views.py
@@ -12,23 +12,7 @@ from django.http import HttpResponseRedirect, QueryDict
 # Create your views here.
 class CustomLoginView(LoginView):
     template_name = "accounts/login.html"
-
-    @method_decorator(sensitive_post_parameters())
-    @method_decorator(csrf_protect)
-    @method_decorator(never_cache)
-    def dispatch(self, request, *args, **kwargs):
-        if self.request.user.is_authenticated:
-            redirect_to = self.get_success_url()
-            return HttpResponseRedirect(redirect_to)
-        if self.redirect_authenticated_user and self.request.user.is_authenticated:
-            redirect_to = self.get_success_url()
-            if redirect_to == self.request.path:
-                raise ValueError(
-                    "Redirection loop for authenticated user detected. Check that "
-                    "your LOGIN_REDIRECT_URL doesn't point to a login page."
-                )
-            return HttpResponseRedirect(redirect_to)
-        return super().dispatch(request, *args, **kwargs)
+    redirect_authenticated_user = True
 
 class CustomLogoutView(LogoutView):
     pass
