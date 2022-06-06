e9042bf8eb323311c4c1cd898bfa8c3c1390cfa1
snphan <snphan@ualberta.ca>
2022-06-05 18:42:45 -0600

    add redirects for login and logout to the settings

--- ReHM_dashboard/ReHM_dashboard/settings.py
+++ ReHM_dashboard/ReHM_dashboard/settings.py
@@ -13,6 +13,7 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 import sys
+from django.urls import reverse_lazy
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -178,3 +179,8 @@ MEDIA_ROOT = BASE_DIR.joinpath('media/')
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+# MARK: Authentication
+LOGIN_REDIRECT_URL = reverse_lazy("dashboard:index")
+LOGOUT_REDIRECT_URL = reverse_lazy("index")
+LOGIN_URL = reverse_lazy("accounts:login")
