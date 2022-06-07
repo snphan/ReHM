6e820ecd9f0490cd70016c1b92f9fbb306ed84ca
snphan <snphan@ualberta.ca>
2022-06-06 19:04:18 -0600

    add whitenoise to serve static files on DEBUG False

--- ReHM_dashboard/ReHM_dashboard/settings.py
+++ ReHM_dashboard/ReHM_dashboard/settings.py
@@ -60,6 +60,7 @@ CHANNEL_LAYERS = {
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
