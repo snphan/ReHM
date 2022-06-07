e14977ab3204ac19941b4dd41d8d2df273c22304
snphan <snphan@ualberta.ca>
2022-06-06 19:05:17 -0600

    setup sass to work with react

--- ReHM_dashboard/accounts/src/js/TestApp.jsx
+++ ReHM_dashboard/accounts/src/js/TestApp.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import {createRoot} from "react-dom/client";
-
+import '../scss/test.scss'
 
 const container = document.getElementById("root");
 const root = createRoot(container);
