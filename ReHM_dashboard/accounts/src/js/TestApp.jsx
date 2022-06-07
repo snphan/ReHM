27f14beb81189a409035b32550795b39efba1a40
snphan <snphan@ualberta.ca>
2022-06-06 21:02:49 -0600

    move the dashboard react app accounts to dahsboard

--- ReHM_dashboard/accounts/src/js/TestApp.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import {createRoot} from "react-dom/client";
-import '../scss/test.scss'
-
-const container = document.getElementById("root");
-const root = createRoot(container);
-root.render(<h1>Hello from React!</h1>);
