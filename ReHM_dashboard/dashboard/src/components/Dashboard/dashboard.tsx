27e103a43a0af7d225271f48c2ad1fcb137c7091
snphan <snphan@ualberta.ca>
2022-06-07 23:51:12 -0600

    add mock chart js data

--- ReHM_dashboard/dashboard/src/components/Dashboard/dashboard.tsx
+++ ReHM_dashboard/dashboard/src/components/Dashboard/dashboard.tsx
@@ -1,10 +1,29 @@
 
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { Line } from "react-chartjs-2";
+import Chart from "chart.js/auto";
+import { CategoryScale } from "chart.js";
 import "./dashboard.scss";
+Chart.register(CategoryScale);
 
 export default function Dashboard() {
     const [showLeft, setShowLeft] = useState(false);
     const [showRight, setShowRight] = useState(false);
+    const [testData, setTestData] = useState({
+        labels: ['Test'],
+        datasets: [{
+            label: 'HR',
+            data: [
+                {x: '2021-11-06 23:39:30', y: 20},
+                {x: '2021-11-06 23:40:30', y: 20},
+                {x: '2021-11-06 23:41:30', y: 20},
+                {x: '2021-11-06 23:42:30', y: 20},
+                {x: '2021-11-06 23:43:30', y: 20},
+            ]
+        }],
+    });
+
+
     const isMobile: boolean = window.innerWidth <= 1024;
     const sidebarWidth: number = 12; // in rem during Desktop
     const sidebarHeight: number = 6; // in rem during Desktop
@@ -29,8 +48,37 @@ export default function Dashboard() {
                     <button data-testid="show-menu" onClick={() => setShowLeft(!showLeft)}>Show left</button>
                     <button data-testid="show-devices" onClick={() => setShowRight(!showRight)}>Show Right</button>
                 </div>
-                <div className="graph-container">
-                    content
+                <div className="graph-container d-flex flex-wrap flex-fill">
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
+                    <div className="m-3" style={{width: "30rem", height: "20rem"}}>
+                        <Line data={testData}/>
+                    </div>
                 </div>
             </div>
             <div data-testid="devices" className={"devices sidebar " + (showRight ? "" : "hidden")}>
