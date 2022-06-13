766508f9ef6049e07f9a87536e3e83ae49ead2ef
snphan <snphan@ualberta.ca>
2022-06-13 16:39:25 -0600

    fix POST assign ReHMUser class to user_id

--- ReHM_dashboard/accounts/serializers.py
+++ ReHM_dashboard/accounts/serializers.py
@@ -1,7 +1,6 @@
 from rest_framework import serializers, validators
 from . import models
-
-
+from rest_framework.utils import html, model_meta
 
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
@@ -16,7 +15,8 @@ class UserSerializer(serializers.ModelSerializer):
 
 class DeviceSerializer(serializers.ModelSerializer):
     user_id = serializers.PrimaryKeyRelatedField(read_only=False,
-                                                queryset=models.ReHMUser.objects.all())
+                                                queryset=models.ReHMUser.objects.all(),
+                                            )
 
     class Meta:
         model = models.Device
@@ -26,6 +26,26 @@ class DeviceSerializer(serializers.ModelSerializer):
             'deviceType',
             'user_id',
         )
+    
+    def create(self, validated_data):
+        user_id = validated_data['user_id'].id
+        validated_data['user_id'] = user_id
+        ModelClass = self.Meta.model
+
+        # Remove many-to-many relationships from validated_data.
+        # They are not valid arguments to the default `.create()` method,
+        # as they require that the instance has already been saved.
+        info = model_meta.get_field_info(ModelClass)
+        many_to_many = {}
+        for field_name, relation_info in info.relations.items():
+            if relation_info.to_many and (field_name in validated_data):
+                many_to_many[field_name] = validated_data.pop(field_name)
+        try:
+            instance = ModelClass._default_manager.create(**validated_data)
+        except TypeError as e:
+            raise TypeError(e)
+        return instance
+
 
 class DeviceTypeSerializer(serializers.ModelSerializer):
     class Meta:
