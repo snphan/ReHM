9b2e4e02d8443b3b5e291b93049c728ded6d7c11
snphan <snphan@ualberta.ca>
2022-06-30 10:49:41 -0600

    auto-format tsx files

--- ReHM_dashboard/dashboard/src/components/LineChart/linechart.tsx
+++ ReHM_dashboard/dashboard/src/components/LineChart/linechart.tsx
@@ -6,34 +6,34 @@ import Chart from "chart.js/auto";
 import 'chartjs-adapter-moment';
 
 interface DataPoint {
-    device: string,             // Apple Watch, Fitbit, Polar, Pozxy 
-    dataType: string,           // HR, RR, ACCEL, GYRO, POS
-    timestamp: number,          // UNIX TIMESTAMP
-    dataValues: Array<number>   // For data that comes as a pack (ACCEL) index 0 = x, 1 = y, 2 = z.
+  device: string,             // Apple Watch, Fitbit, Polar, Pozxy 
+  dataType: string,           // HR, RR, ACCEL, GYRO, POS
+  timestamp: number,          // UNIX TIMESTAMP
+  dataValues: Array<number>   // For data that comes as a pack (ACCEL) index 0 = x, 1 = y, 2 = z.
 }
 
 interface LineChartProps {
-    allData: any,
-    addData: any
+  allData: any,
+  addData: any
 }
 
 export function LineChart(props: LineChartProps) {
-    return (
-        <div data-testid="one-graph">
-            <Line data={props.allData} 
-                options={{
-                    scales: {
-                        x: {
-                            type: 'time',
-                            time: {
-                                unit: 'second',
-                            }
-                        }
-                    },
-                    animation: {
-                        duration: 0
-                    }
+  return (
+    <div data-testid="one-graph">
+      <Line data={props.allData}
+        options={{
+          scales: {
+            x: {
+              type: 'time',
+              time: {
+                unit: 'second',
+              }
+            }
+          },
+          animation: {
+            duration: 0
+          }
 
-                }}/>
-        </div>)
+        }} />
+    </div>)
 }
