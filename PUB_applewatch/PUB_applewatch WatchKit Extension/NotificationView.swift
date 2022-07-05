4d485221dafe9f25088a7474cfa1cb8d9b838ccb
snphan <snphan@ualberta.ca>
2022-07-05 13:44:21 -0600

    start the xcode project for applewatch publisher

---
+++ PUB_applewatch/PUB_applewatch WatchKit Extension/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  PUB_applewatch WatchKit Extension
+//
+//  Created by Steven Phan on 2022-07-05.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
