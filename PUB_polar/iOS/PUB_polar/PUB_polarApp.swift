76ff3d4514d7a7ca6d5d8cb388ca8d6991912247
snphan <snphan@ualberta.ca>
2022-07-07 10:33:32 -0600

    initialize polar pub ios app with pods

---
+++ PUB_polar/iOS/PUB_polar/PUB_polarApp.swift
@@ -0,0 +1,17 @@
+//
+//  PUB_polarApp.swift
+//  PUB_polar
+//
+//  Created by Steven Phan on 2022-07-07.
+//
+
+import SwiftUI
+
+@main
+struct PUB_polarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ PUB_polar/iOS/Pods/Target Support Files/Pods-PUB_polarTests/Pods-PUB_polarTests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_PUB_polarTests : NSObject
+@end
+@implementation PodsDummy_Pods_PUB_polarTests
+@end
